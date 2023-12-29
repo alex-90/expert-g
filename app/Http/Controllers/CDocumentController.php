b4da6397a259cd6769ae887055b940ff57c06a74
Alex Fomichev <alex.fomichev.1990@gmail.com>
2023-12-29 15:43:29 +0600

    migrations added

---
+++ app/Http/Controllers/CDocumentController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\CDocuments;
+use Illuminate\Http\Request;
+
+class CDocumentController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(CDocuments $cDocuments)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(CDocuments $cDocuments)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, CDocuments $cDocuments)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(CDocuments $cDocuments)
+    {
+        //
+    }
+}
---
+++ app/Models/CDocuments.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class CDocuments extends Model
+{
+    protected $table = 'documents';
+
+    use HasFactory;
+}
